bdbbafddbd3d603ed5b36a8e92debc80c9ade41b
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-07-26 10:08:03 +0330

    refactor: update the server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -7,8 +7,8 @@ import { TodoRepository } from "./TodoRepository.js"
 
 const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
   Layer.provide(HttpApiBuilder.middlewareCors()),
-  Layer.provide(HttpApiBuilder.middlewareOpenApi()),
-  Layer.provide(HttpApiSwagger.layer()),
+  Layer.provide(HttpApiBuilder.middlewareOpenApi({ path: "/docs/openapi.json" })),
+  Layer.provide(HttpApiSwagger.layer({ path: "/docs" })),
   Layer.provide(ApiLive),
   Layer.provide(TodoRepository.Default),
   Layer.provide(NodeHttpServer.layer(createServer, { port: 3000 }))
