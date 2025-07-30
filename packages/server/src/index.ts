34297477f962e13a8a1e8c490e81ee84a18eac9d
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-07-30 08:51:01 +0330

    feat: add scalar

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,4 @@
-import { HttpApiBuilder, HttpApiSwagger, HttpMiddleware, HttpServer } from "@effect/platform"
+import { HttpApiBuilder, HttpApiScalar, HttpApiSwagger, HttpMiddleware, HttpServer } from "@effect/platform"
 import { NodeContext, NodeHttpServer, NodeRuntime } from "@effect/platform-node"
 import { Layer } from "effect"
 import { createServer } from "node:http"
@@ -42,7 +42,8 @@ export const ApiLive = HttpApiBuilder.api(Api).pipe(
 
 const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
   Layer.provide(HttpApiBuilder.middlewareCors()),
-  Layer.provide(HttpApiBuilder.middlewareOpenApi({ path: "/docs/openapi.json" })),
+  Layer.provide(HttpApiBuilder.middlewareOpenApi({ path: "/openapi.json" })),
+  Layer.provide(HttpApiScalar.layer({ path: "/references" })),
   Layer.provide(HttpApiSwagger.layer({ path: "/docs" })),
   Layer.provide(ApiLive),
   HttpServer.withLogAddress,
