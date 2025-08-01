0c36a6500cb68f7779151a1ca572964600f2baf4
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-08-01 14:11:16 +0330

    refactor: update cli package

--- packages/cli/src/todo-client.ts
+++ packages/cli/src/todo-client.ts
@@ -11,12 +11,12 @@ export class TodoClient extends Effect.Service<TodoClient>()("cli/TodoClient", {
     })
 
     const create = (text: string) =>
-      client.todo.create({ payload: { text } }).pipe(
+      client.todo.create({ payload: { text }}).pipe(
         Effect.flatMap((todo) => Effect.logInfo("Created todo: ", todo))
       )
 
     const del = (id: TodoId) =>
-      client.todo.delete({ path: { id } }).pipe(
+      client.todo.delete({ path: { id }}).pipe(
         Effect.flatMap(() => Effect.logInfo(`Deleted todo with id: ${id}`)),
         Effect.catchTag("ErrorTodoNotFound", () => Effect.logError(`Failed to find todo with id: ${id}`))
       )
@@ -27,13 +27,13 @@ export class TodoClient extends Effect.Service<TodoClient>()("cli/TodoClient", {
       )
 
     const readById = (id: TodoId) =>
-      client.todo.readById({ path: { id } }).pipe(
+      client.todo.readById({ path: { id }}).pipe(
         Effect.flatMap((todo) => Effect.logInfo(todo)),
         Effect.catchTag("ErrorTodoNotFound", () => Effect.logError(`Failed to find todo with id: ${id}`))
       )
 
     const update = (id: TodoId) =>
-      client.todo.update({ path: { id } }).pipe(
+      client.todo.update({ path: { id }}).pipe(
         Effect.flatMap((todo) => Effect.logInfo("Marked todo done: ", todo)),
         Effect.catchTag("ErrorTodoNotFound", () => Effect.logError(`Failed to find todo with id: ${id}`))
       )
