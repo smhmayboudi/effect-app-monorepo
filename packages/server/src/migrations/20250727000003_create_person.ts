2da19c54de099c5f31c068477476498ae86dc361
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-08-04 22:46:44 +0330

    refactor: update migration

--- packages/server/src/migrations/20250727000003_create_person.ts
+++ packages/server/src/migrations/20250727000003_create_person.ts
@@ -7,24 +7,24 @@ export default Effect.gen(function*() {
     pg: () =>
       sql`CREATE TABLE tbl_person (
       id SERIAL PRIMARY KEY,
-      owner_id INTEGER NOT NULL,
+      group_id INTEGER NOT NULL,
       birthday DATE,
       first_name VARCHAR(255) NOT NULL,
       last_name VARCHAR(255) NOT NULL,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-      FOREIGN KEY (owner_id) REFERENCES tbl_account(id)
+      FOREIGN KEY (group_id) REFERENCES tbl_group(id)
     )`,
     orElse: () =>
       sql`CREATE TABLE tbl_person (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      owner_id INTEGER NOT NULL,
+      group_id INTEGER NOT NULL,
       birthday DATE,
       first_name TEXT NOT NULL,
       last_name TEXT NOT NULL,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
       updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-      FOREIGN KEY (owner_id) REFERENCES tbl_account(id)
+      FOREIGN KEY (group_id) REFERENCES tbl_group(id)
     )`
   })
 })
