1511165a8e297bcd93e15cd460739a0129d2320e
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-08-10 09:31:02 +0330

    refactor: update urlparams

--- packages/domain/src/shared/adapter/URLParams.ts
+++ packages/domain/src/shared/adapter/URLParams.ts
@@ -1,13 +1,24 @@
 import { Schema } from "effect"
 
-export const URLParams = <S extends Schema.Schema<any>>(
-  schema: S
-) => {
+export const URLParams = <S extends Schema.Schema<any>>(schema: S) => {
   const KeysUnion = Schema.keyof(schema)
   const isField = (key: string) => Schema.is(KeysUnion)(key)
   const keys = Object.keys((schema as any).fields).join(", ")
 
   const inputSchema = Schema.Struct({
+    expands: Schema.optional(Schema.String.pipe(
+      Schema.filter(
+        (s) => s.split(",").map((v) => v.trim()).filter(Boolean).every(isField),
+        {
+          description: `Fields must be a comma-separated list of: ${keys}`
+        }
+      ),
+      Schema.annotations({
+        jsonSchema: {
+          description: "Auto expand record relations"
+        }
+      })
+    )),
     fields: Schema.optional(Schema.String.pipe(
       Schema.filter(
         (s) => s.split(",").map((v) => v.trim()).filter(Boolean).every(isField),
@@ -17,14 +28,48 @@ export const URLParams = <S extends Schema.Schema<any>>(
       ),
       Schema.annotations({
         jsonSchema: {
-          description: `Comma-separated list of fields to include: ${keys}`
+          description:
+            "Comma separated string of the fields to return in the JSON response (by default returns all fields)"
+        }
+      })
+    )),
+    limit: Schema.optional(Schema.NumberFromString.pipe(
+      Schema.greaterThan(0),
+      Schema.lessThanOrEqualTo(20),
+      Schema.annotations({
+        description: "The max returned records per request"
+      })
+    )),
+    offset: Schema.optional(Schema.NumberFromString.pipe(
+      Schema.greaterThanOrEqualTo(0),
+      Schema.annotations({
+        description: "The offset returned records per request"
+      })
+    )),
+    sort: Schema.optional(Schema.String.pipe(
+      Schema.filter(
+        (s) => s.split(",").map((v) => v.trim().replace(/^-/, "")).filter(Boolean).every(isField),
+        {
+          description: `Sort must be a comma-separated list of fields (optionally prefixed with '-'): ${keys}`
+        }
+      ),
+      Schema.annotations({
+        jsonSchema: {
+          description: "Specify the ORDER BY fields, add - / + (default) in front of the attribute for DESC / ASC order"
         }
       })
     ))
   })
 
   const outputSchema = Schema.Struct({
-    fields: Schema.optional(Schema.Array(KeysUnion))
+    expands: Schema.optional(Schema.Array(KeysUnion)),
+    fields: Schema.optional(Schema.Array(KeysUnion)),
+    limit: Schema.optional(Schema.Number),
+    offset: Schema.optional(Schema.Number),
+    sort: Schema.optional(Schema.Array(Schema.Struct({
+      by: KeysUnion,
+      sort: Schema.Literal("ASC", "DESC")
+    })))
   })
 
   return Schema.transform(
@@ -32,13 +77,27 @@ export const URLParams = <S extends Schema.Schema<any>>(
     outputSchema,
     {
       decode: (fromA, _fromI) => ({
+        expands: fromA.expands?.split(",")
+          .map((v) => v.trim())
+          .filter(Boolean)
+          .filter(isField),
         fields: fromA.fields?.split(",")
           .map((v) => v.trim())
           .filter(Boolean)
-          .filter(isField)
+          .filter(isField),
+        limit: fromA.limit,
+        offset: fromA.offset,
+        sort: fromA.sort?.split(",").map((v) => ({
+          by: v.replace(/^-/, ""),
+          sort: v.includes("-") ? "DESC" as const : "ASC" as const
+        }))
       }),
       encode: (toI, _toA) => ({
-        fields: toI.fields?.join(",")
+        expands: toI.expands?.join(","),
+        fields: toI.fields?.join(","),
+        limit: toI.limit,
+        offset: toI.offset,
+        sort: toI.sort?.map((v) => `${v.sort === "DESC" ? "-" : ""}${String(v.by)}`).join(",")
       }),
       strict: true
     }
--- packages/server/src/shared/adapter/URLParams.ts
+++ packages/server/src/shared/adapter/URLParams.ts
@@ -1,5 +1,17 @@
 import type { SqlClient } from "@effect/sql"
 import type { URLParams } from "@template/domain/shared/adapter/URLParams"
 
-export const buildSelectQuery = (sql: SqlClient.SqlClient, tableName: string, _urlParams: URLParams) =>
-  sql`SELECT * FROM ${sql(tableName)}`
+export const buildSelectQuery = (sql: SqlClient.SqlClient, tableName: string, urlParams: URLParams) =>
+  urlParams.limit !== undefined && urlParams.offset !== undefined ?
+    urlParams.sort && 0 < urlParams.sort.length
+      ? sql`SELECT * FROM ${sql(tableName)} ORDER BY ${
+        sql.unsafe(urlParams.sort!.map((v) => `${v.by.toString()} ${v.sort}`).join(", "))
+      } LIMIT ${sql.unsafe(urlParams.limit!.toString())} OFFSET ${sql.unsafe(urlParams.offset!.toString())}`
+      : sql`SELECT * FROM ${sql(tableName)} ORDER BY id ASC LIMIT ${sql.unsafe(urlParams.limit!.toString())} OFFSET ${
+        sql.unsafe(urlParams.offset!.toString())
+      }`
+    : urlParams.sort && 0 < urlParams.sort.length
+    ? sql`SELECT * FROM ${sql(tableName)} ORDER BY ${
+      sql.unsafe(urlParams.sort!.map((v) => `${v.by.toString()} ${v.sort}`).join(", "))
+    }`
+    : sql`SELECT * FROM ${sql(tableName)} ORDER BY id ASC`
