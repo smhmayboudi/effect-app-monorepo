595041194e14301697d3e773fb8b9793f736ee6d
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-08-18 18:14:02 +0330

    refactor: update redis

--- packages/server/src/infrastructure/adapter/Redis.ts
+++ packages/server/src/infrastructure/adapter/Redis.ts
@@ -26,8 +26,8 @@ export const RedisTest = makeTestLayer(ResultPersistence)({
       key: ResultPersistence.KeyAny,
       exit: Exit.Exit<unknown, unknown>
     ) => Duration.DurationInput
-  }): Effect.Effect<ResultPersistenceStore, never, Scope.Scope> => {
-    return Effect.gen(function*() {
+  }): Effect.Effect<ResultPersistenceStore, never, Scope.Scope> =>
+    Effect.gen(function*() {
       const scope = yield* Scope.Scope
 
       const store = new Map<string, {
@@ -50,7 +50,9 @@ export const RedisTest = makeTestLayer(ResultPersistence)({
         key: ResultPersistence.KeyAny,
         exit: Exit.Exit<unknown, unknown>
       ) => {
-        if (!options.timeToLive) return undefined
+        if (!options.timeToLive) {
+          return undefined
+        }
         const ttl = options.timeToLive(key, exit)
         return Duration.toMillis(Duration.decode(ttl))
       }
@@ -58,8 +60,8 @@ export const RedisTest = makeTestLayer(ResultPersistence)({
       return {
         get: <R, IE, E, IA, A>(
           key: ResultPersistence.Key<R, IE, E, IA, A>
-        ) => {
-          return Effect.sync(() => {
+        ) =>
+          Effect.sync(() => {
             const entry = store.get(JSON.stringify(key))
             if (!entry) return Option.none()
 
@@ -69,13 +71,12 @@ export const RedisTest = makeTestLayer(ResultPersistence)({
             }
 
             return Option.some(entry.value as Exit.Exit<A, E>)
-          })
-        },
+          }),
 
         getMany: <R, IE, E, IA, A>(
           keys: ReadonlyArray<ResultPersistence.Key<R, IE, E, IA, A>>
-        ) => {
-          return Effect.sync(() => {
+        ) =>
+          Effect.sync(() => {
             return keys.map((key) => {
               const entry = store.get(JSON.stringify(key))
               if (!entry) return Option.none()
@@ -87,35 +88,45 @@ export const RedisTest = makeTestLayer(ResultPersistence)({
 
               return Option.some(entry.value as Exit.Exit<A, E>)
             })
-          })
-        },
+          }),
 
         set: <R, IE, E, IA, A>(
           key: ResultPersistence.Key<R, IE, E, IA, A>,
           value: Exit.Exit<A, E>
-        ) => {
-          return Effect.sync(() => {
+        ) =>
+          Effect.sync(() => {
             const ttl = getExpiration(key, value)
             const expiresAt = ttl !== undefined ? Date.now() + ttl : undefined
             store.set(JSON.stringify(key), {
               value,
               expiresAt
             })
-          })
-        },
+          }),
+
+        setMany: <R, IE, E, IA, A>(
+          entries: Iterable<readonly [ResultPersistence.Key<R, IE, E, IA, A>, Exit.Exit<A, E>]>
+        ) =>
+          Effect.sync(() => {
+            for (const [key, value] of entries) {
+              const ttl = getExpiration(key, value)
+              const expiresAt = ttl !== undefined ? Date.now() + ttl : undefined
+              store.set(JSON.stringify(key), {
+                value,
+                expiresAt
+              })
+            }
+          }),
 
         remove: <R, IE, E, IA, A>(
           key: ResultPersistence.Key<R, IE, E, IA, A>
-        ) => {
-          return Effect.sync(() => {
+        ) =>
+          Effect.sync(() => {
             store.delete(JSON.stringify(key))
-          })
-        },
+          }),
 
         clear: Effect.sync(() => {
           store.clear()
         })
       }
     })
-  }
 })
