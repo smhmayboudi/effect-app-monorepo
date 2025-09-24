7ddaeeaf0e3541abdbe6d66ad0aba48da1809fc2
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-24 09:08:07 +0330

    refactor: update opentelemetry

---
+++ packages/web/instrumentation.ts
@@ -0,0 +1,16 @@
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from "@opentelemetry/semantic-conventions";
+import { registerOTel } from "@vercel/otel";
+
+export function register() {
+  registerOTel({
+    attributes: [
+      {
+        [ATTR_SERVICE_NAME]: "web",
+        [ATTR_SERVICE_VERSION]: "0.0.0",
+      },
+    ],
+  });
+}
