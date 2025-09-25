a4833180dd4707d7c7729f510d2f1fae69bae6a4
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-25 10:39:47 +0330

    refactor: update home

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,10 +1,3 @@
-import type { Metadata } from "next";
-
-// export const metadata: Metadata = {
-//   title: "index",
-//   description: "index",
-// };
-
 export default async function Page() {
   return (
     <div>
