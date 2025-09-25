1be4947d54fa9ebab25c632709ae8473398bb818
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-25 17:34:34 +0330

    feat: add vitest

---
+++ packages/web/vitest.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, mergeConfig } from "vitest/config";
+import shared from "../../vitest.shared.js";
+
+const config = defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "jsdom",
+    exclude: ["**/e2e/**", "**/node_modules/**"],
+    include: ["**/*.test.{ts,tsx}"],
+  },
+});
+
+export default mergeConfig(shared, config);
