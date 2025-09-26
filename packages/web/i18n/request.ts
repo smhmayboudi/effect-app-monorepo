98c3b813196f6d2e6b4ff6a121055d168f03af08
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-26 11:04:03 +0330

    refactor: update typesafe

--- packages/web/i18n/request.ts
+++ packages/web/i18n/request.ts
@@ -1,8 +1,30 @@
-import { hasLocale, IntlErrorCode } from "next-intl";
+import { type Formats, hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 import messagesDefault from "../message/en.json";
 import { headers } from "next/headers";
+
+export const formats = {
+  dateTime: {
+    short: {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    },
+  },
+  number: {
+    precise: {
+      maximumFractionDigits: 2,
+    },
+  },
+  list: {
+    locale: {
+      localeMatcher: "lookup",
+      style: "narrow",
+      type: "unit",
+    },
+  },
+} satisfies Formats;
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const requested = await requestLocale;
@@ -14,27 +36,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
   const timeZone = (await headers()).get("x-time-zone") ?? "Etc/UCT";
 
   return {
-    formats: {
-      dateTime: {
-        short: {
-          day: "numeric",
-          month: "short",
-          year: "numeric",
-        },
-      },
-      number: {
-        precise: {
-          maximumFractionDigits: 2,
-        },
-      },
-      list: {
-        locale: {
-          localeMatcher: "lookup",
-          style: "narrow",
-          type: "unit",
-        },
-      },
-    },
+    formats,
     getMessageFallback: ({ error, key, namespace }) =>
       `'getMessageFallback' called for ${[namespace, key]
         .filter((part) => part != null)
