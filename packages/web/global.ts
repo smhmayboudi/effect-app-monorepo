146460a0d244064df23dc2033ac01c7189b73346
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-26 22:40:17 +0330

    refactor: update global

--- packages/web/global.ts
+++ packages/web/global.ts
@@ -1,7 +1,11 @@
+import { routing } from "@/i18n/routing";
 import { formats } from "@/i18n/request";
+import messages from "@/message/en.json";
 
 declare module "next-intl" {
   interface AppConfig {
     Formats: typeof formats;
+    Locale: (typeof routing.locales)[number];
+    Messages: typeof messages;
   }
 }
