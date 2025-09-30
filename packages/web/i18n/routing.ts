0386a2e53f8a519a5ff67228ddfd3c430ba39e1a
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-30 16:37:12 +0330

    refactor: update i18n cookie config

--- packages/web/i18n/routing.ts
+++ packages/web/i18n/routing.ts
@@ -3,7 +3,11 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   defaultLocale: "fa",
   localeCookie: {
+    // httpOnly: false,
     name: "__next_locale",
+    path: "/",
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
   },
   locales: ["en", "fa"],
 });
