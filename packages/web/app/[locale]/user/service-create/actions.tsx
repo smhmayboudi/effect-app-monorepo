763dd0f58846ca3b22dccce079f90c7a66186b2a
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-09-30 08:23:57 +0330

    refactor: update service create

---
+++ packages/web/app/[locale]/user/service-create/actions.tsx
@@ -0,0 +1,59 @@
+import { HttpClient } from "@/util/http-client";
+import { Registry } from "@effect-atom/atom-react";
+import { IdempotencyKeyClient } from "@template/domain/shared/application/IdempotencyKeyClient";
+import { Effect, Schema } from "effect";
+import { v7 } from "uuid";
+
+type FormState = {
+  errors?: {
+    name?: string[];
+  };
+  message?: string;
+} | null;
+
+export async function serviceCreate(state: FormState, formData: FormData) {
+  const UserSchemaUpdate = Schema.Struct({
+    name: Schema.NonEmptyString,
+  });
+  const program = Schema.decodeUnknown(UserSchemaUpdate)(
+    Object.fromEntries(formData)
+  ).pipe(
+    Effect.flatMap(({ name }) => {
+      const registry = Registry.make();
+      const createMutation = HttpClient.mutation("service", "create");
+      registry.set(createMutation, {
+        headers: { "idempotency-key": IdempotencyKeyClient.make(v7()) },
+        payload: { name },
+        reactivityKeys: ["services"],
+      });
+
+      return Registry.getResult(registry, createMutation, {
+        suspendOnWaiting: true,
+      }).pipe(
+        Effect.map(
+          () =>
+            ({
+              message: "Service create successfully!",
+            } as FormState)
+        )
+      );
+    }),
+    Effect.catchAll((error) => {
+      const errorMessage = error.message.toLowerCase();
+      if (errorMessage.includes("name")) {
+        return Effect.succeed({
+          errors: {
+            name: ["Please enter your name"],
+          },
+          message: "Please check your input and try again.",
+        } as FormState);
+      }
+
+      return Effect.succeed({
+        message: `Failed to sign up. Please try again. ${error.message}`,
+      } as FormState);
+    })
+  );
+
+  return Effect.runPromise(program);
+}
--- packages/web/app/[locale]/user/service-create/client.tsx
+++ packages/web/app/[locale]/user/service-create/client.tsx
@@ -1,42 +1,14 @@
 "use client";
 
-import { useAtomSet } from "@effect-atom/atom-react";
-import { IdempotencyKeyClient } from "@template/domain/shared/application/IdempotencyKeyClient";
 import { useActionState, useState } from "react";
-import { v7 } from "uuid";
-import { HttpClient } from "@/util/http-client";
 import { useTranslations } from "next-intl";
 import Button from "@/component/ui/button";
+import { serviceCreate } from "./actions";
+import useAuth from "@/hook/use-auth";
 
 export default function Client() {
   const t = useTranslations("user.service-create");
-  const httpClientCreate = HttpClient.mutation("service", "create");
-  const createPromise = useAtomSet(httpClientCreate, { mode: "promise" });
-
-  type FormState = {
-    errors?: {
-      name?: string[];
-    };
-    message?: string;
-  } | null;
-
-  const serviceCreate = async (state: FormState, formData: FormData) => {
-    const name = formData.get("name") as string;
-    try {
-      const result = await createPromise({
-        headers: { "idempotency-key": IdempotencyKeyClient.make(v7()) },
-        payload: { name },
-        reactivityKeys: ["services"],
-      });
-      return {
-        message: `Service ${result.data} cerated successfully!`,
-      } as FormState;
-    } catch (error) {
-      return {
-        message: `Failed to create service: ${error}`,
-      } as FormState;
-    }
-  };
+  const { loading, session } = useAuth();
 
   const [state, action, pending] = useActionState(serviceCreate, null);
   const [name, setName] = useState("");
@@ -44,43 +16,49 @@ export default function Client() {
   return (
     <div>
       <h2>{t("title")}</h2>
-      <form action={action}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            aria-disabled={pending}
-            disabled={pending}
-            id="name"
-            name="name"
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Name"
-            required
-            type="text"
-            value={name}
-          />
-        </div>
-        {state?.errors?.name && (
-          <div style={{ color: "red" }}>
-            {state.errors.name.map((error, index) => (
-              <p key={index}>{error}</p>
-            ))}
+      {loading ? (
+        <div>LOADING...</div>
+      ) : !session ? (
+        <p>No user session found. Please log in.</p>
+      ) : (
+        <form action={action}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              aria-disabled={pending}
+              disabled={pending}
+              id="name"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+              required
+              type="text"
+              value={name}
+            />
           </div>
-        )}
-        <Button type="submit" disabled={pending}>
-          {pending ? "Submitting..." : "Submit"}
-        </Button>
-        {state?.message && (
-          <p
-            aria-live="polite"
-            role="status"
-            style={{
-              color: state.message.includes("successfully") ? "green" : "red",
-            }}
-          >
-            {state.message}
-          </p>
-        )}
-      </form>
+          {state?.errors?.name && (
+            <div style={{ color: "red" }}>
+              {state.errors.name.map((error, index) => (
+                <p key={index}>{error}</p>
+              ))}
+            </div>
+          )}
+          <Button type="submit" disabled={pending}>
+            {pending ? "Submitting..." : "Submit"}
+          </Button>
+          {state?.message && (
+            <p
+              aria-live="polite"
+              role="status"
+              style={{
+                color: state.message.includes("successfully") ? "green" : "red",
+              }}
+            >
+              {state.message}
+            </p>
+          )}
+        </form>
+      )}
     </div>
   );
 }
