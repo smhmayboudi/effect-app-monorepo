9744fb20b0fe5c26535cc22c816021fcb5c18010
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-10-01 08:41:00 +0330

    refactor: update graceful shutdown

--- packages/workflow/src/Runner.ts
+++ packages/workflow/src/Runner.ts
@@ -1,9 +1,16 @@
 import { ClusterWorkflowEngine } from "@effect/cluster"
 import { NodeClusterRunnerSocket, NodeRuntime } from "@effect/platform-node"
-import { Config, Layer, Logger, LogLevel } from "effect"
+import { Config, Effect, Layer, Logger, LogLevel } from "effect"
 import { ClientConfigLive } from "./Config.js"
 import { Sql } from "./Sql.js"
 import { WorkflowSendEmailLayer } from "./WorkflowSendEmail.js"
+
+const gracefulShutdown = <A, E, R>(layer: Layer.Layer<A, E, R>) =>
+  Layer.scopedDiscard(
+    Effect.addFinalizer(() => Effect.logInfo("Graceful Shutdown"))
+  ).pipe(
+    Layer.provideMerge(layer)
+  )
 
 Layer.mergeAll(
   WorkflowSendEmailLayer,
@@ -12,6 +19,7 @@ Layer.mergeAll(
   Layer.provide(ClusterWorkflowEngine.layer),
   Layer.provide(NodeClusterRunnerSocket.layer({ storage: "sql" })),
   Layer.provide(Sql(ClientConfigLive.pipe(Config.map((options) => options.Sqlite)))),
+  gracefulShutdown,
   Layer.launch,
   NodeRuntime.runMain
 )
--- packages/workflow/src/ShardManager.ts
+++ packages/workflow/src/ShardManager.ts
@@ -4,6 +4,13 @@ import { Config, Effect, Layer, Logger, LogLevel } from "effect"
 import { ServerConfigLive } from "./Config.js"
 import { Sql } from "./Sql.js"
 
+const gracefulShutdown = <A, E, R>(layer: Layer.Layer<A, E, R>) =>
+  Layer.scopedDiscard(
+    Effect.addFinalizer(() => Effect.logInfo("Graceful Shutdown"))
+  ).pipe(
+    Layer.provideMerge(layer)
+  )
+
 Layer.mergeAll(
   Layer.unwrapEffect(ServerConfigLive.pipe(Effect.map((config) =>
     NodeClusterShardManagerSocket.layer({
@@ -20,6 +27,7 @@ Layer.mergeAll(
 ).pipe(
   Layer.provide(Sql(ServerConfigLive.pipe(Config.map((options) => options.Sqlite)))),
   Layer.orDie,
+  gracefulShutdown,
   Layer.launch,
   NodeRuntime.runMain
 )
