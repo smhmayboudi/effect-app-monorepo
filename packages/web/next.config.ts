a0a7447903886e13d5c1d6486efa2d8c23c12212
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-10-03 10:36:02 +0330

    build: update package json

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
   output: "standalone",
   removeConsole: {
     exclude: ["debug", "error"],
