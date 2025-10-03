fbcce640cac3304aaf24a081b8d778eecf3ad961
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-10-03 13:40:23 +0330

    refactor: update email verification

--- packages/web/app/[locale]/email-verification/client.tsx
+++ packages/web/app/[locale]/email-verification/client.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 
-export default function Client() {
+interface ClientProps {
+  email: string;
+}
+
+export default function Client({ email }: ClientProps) {
   const t = useTranslations("email-verification");
 
   return (
     <div>
       <h2>{t("title")}</h2>
+      <p>{email}</p>
     </div>
   );
 }
--- packages/web/app/[locale]/email-verification/page.tsx
+++ packages/web/app/[locale]/email-verification/page.tsx
@@ -2,8 +2,12 @@ import type { Metadata } from "next";
 import Client from "./client";
 import { getTranslations } from "next-intl/server";
 
+interface PageProps {
+  searchParams: Promise<{ email: string }>;
+}
+
 export async function generateMetadata(): Promise<Metadata> {
-  const t = await getTranslations("forgot-password");
+  const t = await getTranslations("email-verification");
 
   return {
     title: t("title"),
@@ -11,6 +15,8 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function Page() {
-  return <Client />;
+export default async function Page(props: PageProps) {
+  const { email } = await props.searchParams;
+
+  return <Client email={email} />;
 }
