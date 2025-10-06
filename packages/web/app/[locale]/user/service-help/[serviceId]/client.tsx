bf6457b753dca0be1f63b816d2756890e884231e
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-10-06 09:16:43 +0330

    refactor: update service help

--- packages/web/app/[locale]/user/service-help/[serviceId]/client.tsx
+++ packages/web/app/[locale]/user/service-help/[serviceId]/client.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
 import { useTranslations } from "next-intl";
+import Link from "@/component/ui/link";
+import { LuExternalLink } from "react-icons/lu";
 
 interface ClientProps {
   serviceId: string;
@@ -21,7 +22,7 @@ export default function Client({ serviceId }: ClientProps) {
         <p>No user session found. Please log in.</p>
       ) : (
         <Link href={`http://127.0.0.1:3001/auth/${serviceId}/reference`}>
-          Reference
+          Reference <LuExternalLink />
         </Link>
       )}
     </div>
